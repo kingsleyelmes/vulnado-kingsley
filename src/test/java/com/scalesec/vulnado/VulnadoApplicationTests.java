d4b6e8805c5754ba3627a3476d9403f0594939f2
kingsleyelmes <kingsleyelmes@gmail.com>
2025-09-19 11:07:09 +0100

    Add files via upload

---
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.scalesec.vulnado;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class VulnadoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
